dbeb49b8a70ae00c309914b4c7a5ba21c18faeff
McLean Goldwhite <goldwhit@usc.edu>
2015-04-24 18:07:03 -0700

    AI Files
    Files for making the AI controllers and system

---
+++ core/src/com/mygdx/game/components/AIControllerComponent.java
@@ -0,0 +1,18 @@
+package com.mygdx.game.components;
+
+import com.badlogic.ashley.core.Component;
+import com.badlogic.gdx.utils.Pool;
+
+/**
+ * Created by McLean on 4/24/2015.
+ */
+public class AIControllerComponent extends Component implements Pool.Poolable {
+    public int h[][];
+
+    @Override
+    public void reset() {
+
+    }
+
+
+}
---
+++ core/src/com/mygdx/game/systems/AISystem.java
@@ -0,0 +1,32 @@
+package com.mygdx.game.systems;
+
+import com.badlogic.ashley.core.ComponentMapper;
+import com.badlogic.ashley.core.Entity;
+import com.badlogic.ashley.core.Family;
+import com.badlogic.ashley.systems.IteratingSystem;
+import com.mygdx.game.components.AIControllerComponent;
+import com.mygdx.game.dungeon.DungeonGenerator;
+
+/**
+ * Created by McLean on 4/23/2015.
+ */
+public class AISystem extends IteratingSystem {
+
+    private ComponentMapper<AIControllerComponent> aim = ComponentMapper.getFor(AIControllerComponent.class);
+
+    public AISystem(Family family) {
+        super(family);
+    }
+
+    @Override
+    protected void processEntity(Entity entity, float deltaTime) {
+        AIControllerComponent ai = aim.get(entity);
+        int dim = DungeonGenerator.mapSize/2;
+        for(int i = 0; i < dim; i++){
+            for(int j = 0; j < dim; j++){
+            //ai.h
+            }
+        }
+
+    }
+}
